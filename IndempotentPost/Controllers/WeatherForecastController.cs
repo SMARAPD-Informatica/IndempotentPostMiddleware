4e1deb8ccfe1db614d73314c5cb9d949e2eeb8e9
Rafael Augusto Melo de Souza <tr.rafael.ams@gmail.com>
2020-10-26 23:43:11 -0300

    Initial Commit Dojo IndempotentPost 2020-10-24

---
+++ IndempotentPost/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IndempotentPost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        [Route("get")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpPost]
+        [Route("post")]
+        public async Task<IActionResult> Post([FromBody]object dto)
+        {
+            await Task.Delay(TimeSpan.FromMinutes(2));
+            return Ok("Req Ok!");
+        }
+    }
+}
---
+++ IndempotentPost/IndempotentPostMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndempotentPost
+{
+    public class IndempotentPostMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public IndempotentPostMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IMemoryCache cache)
+        {
+            var url = context.Request.Path;
+            var user = context.User.Identity.Name;
+            string cacheKey = $"{user}_{url}";
+            if (context.Request.Method == HttpMethods.Post)
+            {
+                context.Request.EnableBuffering();
+                var request = context.Request;
+                var stream = new StreamReader(request.Body);
+                var body = await stream.ReadToEndAsync();
+                context.Request.Body.Position = 0;
+
+                var hash = body.GetHashCode();
+
+                if (cache.TryGetValue(cacheKey, out int value) && value == hash)
+                {
+                    throw new Exception("Requisição negada.");
+                }
+                else
+                {
+                    cache.Set(cacheKey, hash, TimeSpan.FromMinutes(2));
+                }
+            }
+
+            await _next.Invoke(context);
+
+            if (cache.TryGetValue(cacheKey, out _))
+            {
+                cache.Remove(cacheKey);
+            }
+
+        }
+    }
+
+}
